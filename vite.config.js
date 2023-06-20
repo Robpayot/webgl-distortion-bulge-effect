12040dd7432921a34e01510c20ec0e5eb173de15
Robin payot <robpayot@gmail.com>
2023-06-20 10:18:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import * as path from 'path'
 
 export default defineConfig({
   root: '',
-  base: '/vite-2d-shader-template/', // for Github pages, otherwise use './'
+  base: '/webgl-distortion-bulge-effect/', // for Github pages, otherwise use './'
   build: {
     outDir: 'dist',
   },
